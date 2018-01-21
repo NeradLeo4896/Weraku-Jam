b77215197d6e55387d97923f225be89514d057d7
NeradLeo4896 <d.jay4896@gmail.com>
2018-01-21 16:13:37 +0800

    Added waters.

--- Weraku Jam/Assets/Scripts/Player.cs
+++ Weraku Jam/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 	#region Declarations
 
 	Animator anim;
+	Rigidbody2D rb;
+	public float jumpSpeed = 1.0f;
 
 	#endregion
 
@@ -14,11 +16,14 @@ public class Player : MonoBehaviour
 	void Start ()
 	{
 		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y + jumpSpeed);
+		}
 	}
 }
